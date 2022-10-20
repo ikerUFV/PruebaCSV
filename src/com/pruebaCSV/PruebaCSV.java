3d966904c6dc8adc8cce62b487daa627dcb1e634
Iker Rodríguez <9005659@alumnos.ufv.es>
2022-10-20 21:09:34 +0200

    Metida libreria OpenCSV y todo funcionando commit

--- src/com/pruebaCSV/PruebaCSV.java
+++ src/com/pruebaCSV/PruebaCSV.java
@@ -2,6 +2,7 @@ package com.pruebaCSV;
 
 public class PruebaCSV {
     public static void main(String[] args){
-
+        LectorCSV miLector = new LectorCSV();
+        miLector.leeCSV("./datos/pokemon.csv");
     }
 }
