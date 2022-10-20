0451641151028ce66eef6c1f60e23cc767adfb53
Iker Rodríguez <9005659@alumnos.ufv.es>
2022-10-20 20:51:08 +0200

    Segundo commit

---
+++ src/com/pruebaCSV/LectorCSV.java
@@ -0,0 +1,21 @@
+package com.pruebaCSV;
+import java.io.FileReader;
+import com.opencsv.CSVReader;
+
+public class LectorCSV {
+    public void leeCSV(String nombreArchivo){
+        try{
+            CSVReader csvReader = new CSVReader(new FileReader(nombreArchivo));
+            String[] fila = null;
+            while ((fila = csvReader.readNext()) != null){
+                System.out.println((fila[0]
+                + " | " + fila[1]
+                + " | " + fila[2]));
+
+            }
+            csvReader.close();
+        }catch (Exception ex){
+            System.out.print(ex.getMessage());
+        }
+    }
+}
